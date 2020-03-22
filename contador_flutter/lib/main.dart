498461be864a0a6a9b906d900bfc645e29a66717
Pedro Satin <pedro5satin@gmail.com>
2020-03-22 14:19:07 -0300

    Finishing

--- contador_flutter/lib/main.dart
+++ contador_flutter/lib/main.dart
@@ -1,48 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-            child: Text(
-              "Pessoas: 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
+void main() => runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  int people = 0;
+
+  void onPressPeople(int p) => this.setState(() => people += p);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Text(
+            "Pessoa(s): $people",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: FlatButton(
-                  child: Text(
-                    "+1",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                  onPressed: () => {},
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
                 ),
+                onPressed: () => onPressPeople(1),
               ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: FlatButton(
-                  child: Text(
-                    "-1",
-                    style: TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                  onPressed: () => {},
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
                 ),
-              )
-            ],
-          ),
-          Text(
-            "Pode Entrar!",
-            style: TextStyle(
-                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
-          )
-        ],
-      ),
-    ));
+                onPressed: () => onPressPeople(-1),
+              ),
+            )
+          ],
+        ),
+        Text(
+          "Pode Entrar!",
+          style: TextStyle(
+              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+        )
+      ],
+    );
+  }
+}
