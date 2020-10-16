a788870fea47be41fbbbac7ff4f89449483197b4
lucasvaldemar92 <72977066+lucasvaldemar92@users.noreply.github.com>
2020-10-16 08:46:44 -0300

    Update common.js

--- src/common.js
+++ src/common.js
@@ -3,7 +3,7 @@ import { Alert, Platform } from 'react-native';
 const server = Platform.OS === 'IOS' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
 
 function showError(error) {
-    Alert.alert('Ops! Houve um erro!', `Erro: ${error}`);
+    Alert.alert('Ops! Houve um erro!', `Erro! : ${error}`);
 }
 
 function showSuccess(msg) {
